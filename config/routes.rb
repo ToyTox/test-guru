ec9b74f72be00b5d4525089108f4341e2f594597
Artem Kosulin <artem.kosulin@flant.com>
2026-04-13 23:47:21 +0300

    Add about/author page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'page/about'
+
+  get 'about/author'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
