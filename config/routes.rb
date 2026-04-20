ef3489d02107263917f86b3edd8f9bb2ab193da3
Artem <45466149+ToyTox@users.noreply.github.com>
2026-04-20 22:20:18 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'about' => 'page#about', as: :about
+  get 'about' => 'page#about', as: :page_about
 
   get 'about/author' => 'page#author', as: :author
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
