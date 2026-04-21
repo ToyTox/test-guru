48aba8ce1644662c813167b58e9606ef3f32a8fa
Artem Kosulin <artem.kosulin@flant.com>
2026-04-21 23:07:29 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'about' => 'page#about', as: :page_about
 
-  get 'about/author' => 'page#author', as: :author
+  get 'about/author' => 'page#author', as: :page_author
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
