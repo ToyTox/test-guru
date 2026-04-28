a52d9d27e47b2180ec9cf5b5573c503486561750
Artem Kosulin <artem.kosulin@flant.com>
2026-04-28 23:45:46 +0300

    Add tests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_27_204249) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_28_203500) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
+    t.integer "level"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  add_foreign_key "tests", "categories"
 end
