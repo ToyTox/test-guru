1da0b13126c2d78fa63903b3933d02b6d126f330
Artem Kosulin <artem.kosulin@flant.com>
2026-04-29 23:34:00 +0300

    Add questions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_28_203500) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_29_201626) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "body"
+    t.integer "test_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -26,5 +34,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_28_203500) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
