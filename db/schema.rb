688a708cc3066da5dfcd672c255ad5cbbc20b2cf
Artem Kosulin <artem.kosulin@flant.com>
2026-04-30 21:40:12 +0300

    Add answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_29_201626) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_30_183852) do
+  create_table "answers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
