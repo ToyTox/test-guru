b421e40c76ca870ef4629938e8299ce72890a3b3
Artem Kosulin <artem.kosulin@flant.com>
2026-05-01 23:26:54 +0300

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_30_183852) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_01_202328) do
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,6 +37,11 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_30_183852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "questions", "tests"
