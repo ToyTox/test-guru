27d1579ac1840c90b88d594b5c98f66fa844babc
Artem Kosulin <artem.kosulin@flant.com>
2026-05-03 23:43:50 +0300

    Add questions body null constraint

---
+++ db/migrate/20260503203458_add_question_body_null_constraint.rb
@@ -0,0 +1,9 @@
+class AddQuestionBodyNullConstraint < ActiveRecord::Migration[7.1]
+  def up
+    change_column_null(:questions, :body, false)
+  end
+  
+  def down
+    change_column_null(:questions, :body, true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_02_204014) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_03_203458) do
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_02_204014) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
