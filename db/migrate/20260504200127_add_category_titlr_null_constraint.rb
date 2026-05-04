466dbd7c10192a78c3ef022e1411aa62029c1a49
Artem Kosulin <artem.kosulin@flant.com>
2026-05-04 23:09:59 +0300

    Add categies title null constraint

---
+++ db/migrate/20260504200127_add_category_titlr_null_constraint.rb
@@ -0,0 +1,9 @@
+class AddCategoryTitlrNullConstraint < ActiveRecord::Migration[7.1]
+  def up
+    change_column_null(:categories, :title, false)
+  end
+  
+  def down
+    change_column_null(:categories, :title, true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_03_203458) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_04_200127) do
   create_table "answers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
