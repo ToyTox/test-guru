eec7204dfc9d4b1631172298370b17d8d24c18eb
Artem Kosulin <artem.kosulin@flant.com>
2026-06-17 23:27:21 +0300

    Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_many :test_results
+  has_many :users
+  has_many :authors
 
   def self.desc_sorted_tests(category_name)
     joins(:category).where(categories: { title: category_name }).order("tests.title DESC").pluck("tests.title")
