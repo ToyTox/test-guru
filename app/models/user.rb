623fdcc6af32336e87d33ca99f914888166d4831
Artem Kosulin <artem.kosulin@flant.com>
2026-06-19 23:43:01 +0300

    Fix tests and users relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :test_results
   has_many :tests, through: :test_results
+  has_many :tests
 
   def tests_list(level)
     tests.where(level: level).distinct
