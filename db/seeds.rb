f04bcd8abfd6bed9f8d3c355dfd59223c0499d12
Artem Kosulin <artem.kosulin@flant.com>
2026-06-20 23:36:59 +0300

    Remove comments from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # Users
 user1 = User.create!(name: "Тестовый Тест", email: "test@123.com")
 user2 = User.create!(name: "Второй Тест", email: "test2@123.com")
