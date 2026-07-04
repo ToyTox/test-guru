8eeeee27736eebd865f0143e9fd1f437a0f3e8fa
Artem Kosulin <artem.kosulin@flant.com>
2026-07-04 23:28:33 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ user1 = User.create!(name: "Тестовый Тест", email: "test@123.com")
 user2 = User.create!(name: "Второй Тест", email: "test2@123.com")
 user3 = User.create!(name: "Третий Тест", email: "test3@123.com")
 user4 = User.create!(name: "Четвертый Тест", email: "test4@123.com")
+user5 = User.create!(name: "Пятый Тест", email: "test5@123.com")
 
 # Categories
 backend = Category.create!(title: "Backend")
@@ -54,8 +55,8 @@ Answer.create!([
 # Test Resaults
 TestResult.create!([
   { user: user1, test: ruby_test },
-  { user: user1, test: rails_test },
   { user: user2, test: js_test },
   { user: user3, test: mvc_test },
-  { user: user4, test: html_test }
+  { user: user4, test: html_test },
+  { user: user5, test: rails_test }
 ])
