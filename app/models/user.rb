f54b8379c536cc908cf943c21c04e799506bf138
Artem Kosulin <artem.kosulin@flant.com>
2026-08-20 23:41:56 +0300

    Update User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :test_results
   has_many :tests, through: :test_results
-  has_many :tests
 
   def tests_list(level)
     tests.where(level: level).distinct
