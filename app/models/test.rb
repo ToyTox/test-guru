3e10ab4ff992cca8bec33e00abf4864f09bb3a5e
Artem Kosulin <artem.kosulin@flant.com>
2026-08-21 22:54:44 +0300

    Update Test class

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,6 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :test_results
   has_many :users, through: :test_results
-  has_many :users
 
   def self.desc_sorted_tests(category_name)
     joins(:category).where(categories: { title: category_name }).order("tests.title DESC").pluck("tests.title")
