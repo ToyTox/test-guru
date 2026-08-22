8bec8edf06174c5cef568004278bc0c61689c0a7
Artem Kosulin <artem.kosulin@flant.com>
2026-08-22 23:54:18 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,5 +56,6 @@ Answer.create!([
 TestResult.create!([
   { user: user1, test: ruby_test },
   { user: user2, test: js_test },
-  { user: user3, test: mvc_test }
+  { user: user3, test: mvc_test },
+  { user: user4, test: rails_test }
 ])
